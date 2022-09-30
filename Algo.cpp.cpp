12cc1f0e3395200743fefae950fee3ceb20b59fd
Avijit Saha <113923243+Avijitsaha13@users.noreply.github.com>
2022-09-30 16:41:15 +0600

    Add files via upload

---
+++ Algo.cpp.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+srand(time(0));
+freopen("Linear Search","w",stdout);
+int a[1000];
+for(int i=0;i<100;i++)
+{
+    a[i]=rand();
+    cout<<a[i]<<endl;
+}
+
+
+return 0;
+}
